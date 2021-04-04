ddb1622b8be3efedee7511e2492aa2a7049bef4a
880066 <60427423+880066@users.noreply.github.com>
2021-04-04 22:28:35 +0300

    Update projecteuler14.c

--- projecteuler14.c
+++ projecteuler14.c
@@ -1,32 +1,57 @@
+/*  
+The following iterative sequence is defined for the set of positive integers:
+
+n › n/2 (n is even)
+n › 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+
+13 › 40 › 20 › 10 › 5 › 16 › 8 › 4 › 2 › 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+
+*/
+
+
 #include <stdio.h>
 
-int f(int);
+int f(unsigned long long int);
 
 int main(void){
-    
-    int sayac=0,cevap=0,t,i,sinir;
-    sinir=1000000;
-    for (i=sinir/2;i<sinir;i++){
-        t=f(i);
-        if(t > sayac){
-            sayac=t;
-            cevap=i;
-        }
-        
-    }
-    
-    printf("%d",cevap);
-    return 0;
+	
+	int sinir=1000000,cevap=0,sayac=0,i,ff;
+	
+	for(i=500000;i<sinir;i++){
+		
+		if(    ((i*2)-1)/3 % 2 == 1  ){
+			continue;
+		}
+		
+		ff=f(i);
+		
+		if (ff > sayac){
+			sayac=ff;
+			cevap=i;
+		}
+		
+		
+	}
+	
+	printf("%d",cevap);
+	
+	return 0;
 }
 
-int f (int x){
-    
-    int counter =1;
-    while(x != 1 ){
-        
-        counter++;
-        x=(x%2==0) ? x/2 : x*3 +1;
-    }
-    
-    return counter ;
-} 
+int f(unsigned long long int x){
+	
+	int counter =1;
+	while(x != 1){
+		x = (x % 2 == 0) ? x /2 : x*3 +1;
+		counter++;
+	}
+	
+	return counter;
+}
